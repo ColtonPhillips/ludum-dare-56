afc873203ecdc98a6c0def22edeb774fa2c5dc44
Colton Phillips <coltonjphillips@gmail.com>
2024-10-04 19:41:29 -0700

    Finally parsing my game data into a vec of tokens

---
+++ src/creatures.rs
@@ -0,0 +1,30 @@
+use regex::Regex;
+
+#[derive(Debug)]
+pub enum Token {
+    Name(String),
+    Hint(String),
+}
+pub type Tokens = Vec<Token>;
+
+pub fn parse_creatures() -> Tokens {
+    let creatures = include_str!("creatures.txt");
+    let mut tokens: Tokens = Tokens::new();
+    for line in creatures.lines() {
+        match line {
+            "" => {} // split off empty lines in a dumb way
+            _ => {
+                // Extract a hint! if one exists, and tokenize it
+                let re = Regex::new(r#""([^"]+)""#).unwrap();
+                if let Some(caps) = re.captures(line) {
+                    let h = caps.get(1).unwrap().as_str();
+                    tokens.push(Token::Hint(h.to_string()));
+                }
+                // Then extract the line for the creature and trim whitespace
+                let line = re.replace(line, "").to_string();
+                tokens.push(Token::Name(String::from(line.trim())));
+            }
+        }
+    }
+    tokens
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod creatures;
+
+use creatures::parse_creatures;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let tokens = parse_creatures();
+    print!("{tokens:?}");
+    Ok(())
 }
